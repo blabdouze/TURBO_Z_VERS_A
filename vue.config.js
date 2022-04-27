13ddd4dea06bce412bd5eadea0367bdf7b80aa5d
blabdouze <boris.labbe2@gmail.com>
2022-04-27 22:15:45 +0200

    fix: change webpack public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true, 
+  // Raw loader config
+  // https://stackoverflow.com/questions/54697757/read-file-inside-current-directory-using-vue
   chainWebpack: config => {
     config.module
       .rule('raw')
@@ -9,4 +11,7 @@ module.exports = defineConfig({
       .loader('raw-loader')
       .end()
   },
+  // Remove this is you want to run the app on a dedicated server
+  // https://stackoverflow.com/questions/50809987/vue-cli-build-and-run-index-html-file-without-server
+  publicPath: '' 
 })
